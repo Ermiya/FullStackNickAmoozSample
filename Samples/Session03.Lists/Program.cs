2eff76bc0043522b624c1bd80e5b2aefb6b1b4dc
farshid <farshid.saeidi@hotmail.com>
2019-10-09 14:59:02 +0330

    .

---
+++ Samples/Session03.Lists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session03.Lists
+{
+    public class Person : IEquatable<Person>
+    {
+        public string FName { get; set; }
+        public bool Equals(Person other)
+        {
+            return FName == other.FName;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>();
+            List<int> myList01 = new List<int>(250) { 1, 2, 3, 4 };
+
+
+
+            myList01.Add(1);
+            myList01.Remove(1);
+            
+            myList01.RemoveAt(0);
+            myList01.TrimExcess();
+
+            myList01.Find(x => x.ToString() == "");
+
+            myList01.Sort();
+
+            myList01.Count();
+            var a = myList01.Capacity;
+
+            myList01.AsReadOnly();
+
+        }
+    }
+}
---
+++ Samples/Tuppels/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuppels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var tup = Tuple.Create<int, string, int>(1, "", 2);
+            testTup(tup);
+            var fullname = GetFullName();
+            //fullname.age
+
+            //Tupple Deconstruction
+            (int age, string Name, string Family) = GetFullName();
+
+            
+
+            
+
+        }
+        public static (int age, string firstName, string lastName) GetFullName()
+        {
+            return (1, "Farshid", "Saeidi");
+        }
+            
+        private static void testTup(Tuple<int, string, int> tup)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
